e60d586f016267d35415c051b495888f9b645de2
eiwi1101 <weisert@eisertdev.com>
2016-12-31 16:04:09 -0600

    That was a bad idea.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,5 @@ class ApplicationController < ActionController::Base
   include SessionHelper
 
   protect_from_forgery with: :exception
-
-  def self.force_ssl(options = {})
-    host = options.delete(:host)
-    before_filter(options) do
-      if !request.ssl? && !(respond_to?(:allow_http?) && allow_http?)
-        redirect_options = {:protocol => 'https://', :status => :moved_permanently}
-        redirect_options.merge!(:host => host) if host
-        redirect_options.merge!(:params => request.query_parameters)
-        redirect_to redirect_options
-      end
-    end
-  end
-
   force_ssl if Rails.env.production?
 end
