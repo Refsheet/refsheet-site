f066d293865d2299114999359d05c28d0dd9334a
Mau Abata <mauabata@gmail.com>
2016-12-31 18:59:22 -0600

    Registration working!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new user_params
 
+    if @user.save
+      render json: @user
+    else
+      render json: { errors: @user.errors }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
