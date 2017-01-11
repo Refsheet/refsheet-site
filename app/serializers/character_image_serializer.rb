c3c284f2a868a234216a855b586f4daebc3469a2
eiwi1101 <weisert@eisertdev.com>
2017-01-11 12:25:27 -0600

    Image sizing is a thing.

--- app/serializers/character_image_serializer.rb
+++ app/serializers/character_image_serializer.rb
@@ -1,11 +1,19 @@
 class CharacterImageSerializer < ActiveModel::Serializer
-  attributes :id, :url
+  attributes :id, :url, :small, :medium
 
   def id
     object.guid
   end
 
   def url
-    object.image&.url
+    object.image&.url(:large)
+  end
+
+  def small
+    object.image&.url(:small)
+  end
+
+  def medium
+    object.image&.url(:medium)
   end
 end
