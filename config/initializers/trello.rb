233fe4418cbd00d583857edad4a72a68466765c3
Mau Abata <mauabata@gmail.com>
2017-01-19 00:50:12 -0600

    Feedbak form complete.

---
+++ config/initializers/trello.rb
@@ -0,0 +1,8 @@
+Rails.application.configure do
+  Rails.configuration.x.trello = config_for(:trello)
+
+  Trello.configure do |config|
+    config.developer_public_key = Rails.configuration.x.trello['api_key']
+    config.member_token = Rails.configuration.x.trello['token']
+  end
+end
---
+++ spec/models/feedback_spec.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: feedbacks
+#
+#  id             :integer          not null, primary key
+#  user_id        :integer
+#  name           :string
+#  email          :string
+#  comment        :text
+#  trello_card_id :string
+#  source_url     :string
+#  visit_id       :integer
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
+require 'rails_helper'
+
+describe Feedback, type: :model do
+  it 'tells trello' do
+    expect_any_instance_of(Trello::Card).to receive(:save).and_return(true)
+
+    f = Feedback.new name: 'Mau', comment: 'Is testing again.'
+    f.save
+  end
+end
