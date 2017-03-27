09bfbec7d6347e900740c1b2cb4987ebbea1024b
William Eisert <weisert@eisertdev.com>
2017-03-26 22:38:16 -0500

    Fixed the badbug.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,17 +29,17 @@ class ApplicationController < ActionController::Base
 
   def set_user_locale
     session[:locale]   = params[:locale] if params.include? :locale
-    session[:locale] ||= current_user&.settings[:locale]
+    session[:locale] ||= current_user.settings[:locale] if logged_in?
     session[:locale] ||= I18n.default_locale
     I18n.locale = session[:locale]
 
-    Time.zone   = current_user&.settings[:time_zone]
+    Time.zone   = current_user.settings[:time_zone] if logged_in?
     Time.zone ||= Application.config.time_zone
   end
 
   def set_default_meta
     site = 'Refsheet.net'
-    desc = 'A new, convenient way to organize your character designs and art.'
+    desc = 'Show off reference sheets for your characters to help art commissions and share your worlds!'
 
     set_meta_tags(
         site: site,
