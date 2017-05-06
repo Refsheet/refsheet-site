5da9d055745a9020819bd53a18a54ca642538888
William Eisert <weisert@eisertdev.com>
2017-05-05 20:39:44 -0500

    Rejecting doubles :D!

--- app/models/media/favorite.rb
+++ app/models/media/favorite.rb
@@ -6,4 +6,15 @@ class Media::Favorite < ApplicationRecord
 
   validates_presence_of :media
   validates_presence_of :user
+  validate :unique_favorite
+
+  private
+
+  def unique_favorite
+    if Media::Favorite.exists? user: self.user, media: self.media
+      self.errors.add :media, 'has already been liked'
+      false
+    end
+    true
+  end
 end
