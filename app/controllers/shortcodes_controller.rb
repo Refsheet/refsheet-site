f4ac9fc5e99c8caa590632c6ecbf37eb24aa09ca
William Eisert <weisert@eisertdev.com>
2017-05-06 17:40:00 -0500

    Looks like shortcodes fixed.

--- app/controllers/shortcodes_controller.rb
+++ app/controllers/shortcodes_controller.rb
@@ -5,9 +5,13 @@ class ShortcodesController < ApplicationController
     if @character
       redirect_to "https://refsheet.net/#{@character.user.username.downcase}/#{@character.slug.downcase}"
     else
-      Rack::Utils::SYMBOL_TO_STATUS_CODE[:teapot] = 418
-      Rack::Utils::HTTP_STATUS_CODES[418] = "I'm a Teapot"
-      render 'application/teapot', status: :teapot
+      if request.domain == 'ref.st'
+        redirect_to "https://refsheet.net/c/#{params[:id]}"
+      else
+        Rack::Utils::SYMBOL_TO_STATUS_CODE[:teapot] = 418
+        Rack::Utils::HTTP_STATUS_CODES[418] = "I'm a Teapot"
+        render 'application/teapot', status: :teapot
+      end
     end
   end
 end
