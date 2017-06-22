6e54a75005515b965ba455d297026059d192bfad
William Eisert <weisert@eisertdev.com>
2017-06-21 22:12:38 -0500

    Some minor big fixes?

--- app/serializers/user_index_serializer.rb
+++ app/serializers/user_index_serializer.rb
@@ -2,5 +2,15 @@ class UserIndexSerializer < ActiveModel::Serializer
   attributes :name,
              :username,
              :avatar_url,
+             :is_admin,
+             :is_patron,
              :settings
+
+  def is_admin
+    object.role? :admin
+  end
+
+  def is_patron
+    object.pledges.active.any?
+  end
 end
