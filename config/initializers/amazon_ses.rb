cdb427da205e2a6221c24b2f1b13424bed9510b6
Mau Abata <mauabata@gmail.com>
2017-06-22 18:38:07 -0500

    Fixed SES

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-                                       access_key_id: ENV['AMAZON_ACCESS_KEY'],
-                                       secret_access_key: ENV['AMAZON_SECRET_KEY']
+                                       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
