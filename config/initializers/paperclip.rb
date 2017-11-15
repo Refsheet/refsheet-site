fff364395fedd99783ca1396de86e63f67dac344
William Eisert <weisert@eisertdev.com>
2017-11-15 04:45:35 -0600

    Wrapping up campaign fix.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -3,6 +3,9 @@ Rails.application.configure do
       storage: :s3,
       s3_protocol: :https,
       s3_credentials: Rails.configuration.x.amazon
+      # s3_host_alias: Rails.configuration.x.amazon[:s3_host_alias],
+      # url: ':s3_alias_url',
+      # path: '/:class/:attachment/:id_partition/:style/:filename'
   }
 
   Paperclip.interpolates :guid do |attachment, _|
