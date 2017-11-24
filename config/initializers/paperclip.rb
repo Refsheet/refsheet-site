4a81098d3752be5311ef88092b85333fe1e4ef13
William Eisert <weisert@eisertdev.com>
2017-11-24 05:08:55 -0600

    Cloud front!

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.paperclip_defaults = {
+  defaults = {
       storage: :s3,
       s3_protocol: :https,
       s3_credentials: Rails.configuration.x.amazon
@@ -7,6 +7,13 @@ Rails.application.configure do
       # url: ':s3_alias_url',
       # path: '/:class/:attachment/:id_partition/:style/:filename'
   }
+
+  if ENV['CLOUD_DOMAIN']
+    defaults.merge! s3_host_alias: ENV['CLOUD_DOMAIN'],
+                    url: ':s3_alias_url'
+  end
+
+  config.paperclip_defaults = defaults
 
   Paperclip.interpolates :guid do |attachment, _|
     attachment.instance.guid
