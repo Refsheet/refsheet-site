92ed9b827a7ce997ad4cd5379de4d30489c01a45
Mau Abata <mauabata@gmail.com>
2017-11-28 06:59:36 -0600

    Reverting the bundle madness.

--- vendor/cache/react-rails-56eab4bb6d27/react-builds/react-server.js
+++
@@ -1,5 +0,0 @@
-var React = require("react");
-var ReactDOMServer = require("react-dom/server");
-
-global.React = React;
-global.ReactDOMServer = ReactDOMServer;
--- vendor/cache/will_paginate-materialize-86e86a9ea3b2/lib/materialize_pagination/version.rb
+++
@@ -1,3 +0,0 @@
-module MaterializePagination
-  VERSION = "0.1.2"
-end
