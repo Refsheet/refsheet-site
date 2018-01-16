ae4270cc0785c4bf42828e4c935b968eb30969a5
William Eisert <weisert@eisertdev.com>
2018-01-16 13:50:53 -0600

    Moderation report model created.

---
+++ spec/factories/moderation_reports.rb
@@ -0,0 +1,37 @@
+# == Schema Information
+#
+# Table name: moderation_reports
+#
+#  id               :integer          not null, primary key
+#  user_id          :integer
+#  sender_user_id   :integer
+#  moderatable_id   :integer
+#  moderatable_type :string
+#  violation_type   :string
+#  comment          :text
+#  dmca_source_url  :string
+#  status           :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_moderation_reports_on_moderatable_id    (moderatable_id)
+#  index_moderation_reports_on_moderatable_type  (moderatable_type)
+#  index_moderation_reports_on_sender_user_id    (sender_user_id)
+#  index_moderation_reports_on_status            (status)
+#  index_moderation_reports_on_user_id           (user_id)
+#  index_moderation_reports_on_violation_type    (violation_type)
+#
+
+FactoryGirl.define do
+  factory :moderation_report do
+    sender_user_id 1
+    moderatable_id 1
+    moderatable_type "MyString"
+    violation_type "MyString"
+    comment "MyText"
+    dmca_source_url "MyString"
+    status "MyString"
+  end
+end
