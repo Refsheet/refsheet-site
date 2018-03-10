6a736a7ac51e69de4e328edc2eca08039c8e069e
Mau Abata <mauabata@gmail.com>
2018-03-10 12:24:55 -0600

    Webpacker replace for Character page. It's ALIIIIIVE!

---
+++ app/javascript/components/characters/view.coffee
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class CharacterView extends React.Component
+  render: ->
+    `<h1>Character View</h1>`
+
+export default CharacterView
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,22 @@
 const { environment } = require('@rails/webpacker')
+const coffee = require('./loaders/coffee')
+const erb = require('./loaders/erb')
+
+// Export Defaults
+
+let output = {
+  library: ['Packs', 'application'],
+  libraryTarget: 'var'
+}
+
+environment.config.merge({output})
+
+// Drink Coffee
+
+environment.loaders.append('coffee', coffee)
+environment.loaders.append('erb', erb)
+
+const babelLoader = environment.loaders.get('babel')
+babelLoader.test = /\.(coffee|js|jsx)(\.erb)?$/
 
 module.exports = environment
