b19c8455e90597325436fed869689090eacf4a96
William Eisert <weisert@eisertdev.com>
2018-03-24 02:48:05 -0500

    GraphQL and some Karma.

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,19 @@
+Types::QueryType = GraphQL::ObjectType.define do
+  name "Query"
+  # Add root-level fields here.
+  # They will be entry points for queries on your schema.
+
+  field :getCharacter, !Types::CharacterType do
+    argument :id, !types.ID
+    resolve -> (_obj, args, _ctx) { Character.find(args[:id]) }
+  end
+
+  field :getCharacterByUrl, !Types::CharacterType do
+    argument :username, !types.String
+    argument :slug, !types.String
+
+    resolve -> (_obj, args, _ctx) {
+      User.lookup!(args[:username]).characters.lookup!(args[:slug])
+    }
+  end
+end
