62621f3f41b51c08221e9817844e36d7a3cfa758
William Eisert <weisert@eisertdev.com>
2018-08-14 00:02:54 -0500

    ImageUploadFOrm

---
+++ app/javascript/components/Image/UploadForm.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import {
+  Row,
+  Input,
+  Button
+} from 'react-materialize'
+
+class UploadForm extends Component {
+  constructor (props) {
+    super(props)
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    const {
+      image,
+      onChange
+    } = this.props
+
+    let value = e.target.value
+
+    if (e.target.type === 'checkbox') {
+      value = e.target.checked
+      console.log(value)
+    }
+
+    if (!onChange) return
+    image[ e.target.name ] = value
+    onChange(image)
+  }
+
+  render () {
+    const {
+      image
+    } = this.props
+
+    const folders = [
+      [ 'default', 'Default' ],
+      [ 'scraps', 'Scraps' ],
+      [ 'wips', 'WIPs' ]
+    ]
+
+    return <div className='image-form' style={ { flex: '0 1 300px', display: 'flex', height: 300, flexDirection: 'column' } }>
+      <div className='image-data padding--medium padding-bottom--none' style={ { flexGrow: 1 } }>
+        <Row>
+          <Input s={ 12 } onChange={ this.handleChange } value={ image.title } type='text' name='title' className='margin-bottom--none' label='Name'/>
+        </Row>
+        <Row>
+          <Input s={ 12 } onChange={ this.handleChange } value={ image.folder } type='select' name='folder' className='margin-bottom--none' label='Folder'>
+            { folders.map((i) => <option value={ i[ 0 ] } key={ i[ 0 ] }>{ i[ 1 ] }</option>) }
+          </Input>
+        </Row>
+      </div>
+
+      <div className='image-submit padding--medium padding-top--none' style={ { flexShrink: 0, flexGrow: 0 } }>
+        <div className='center margin-bottom--large'>
+          <Row>
+            <Input s={ 12 } onChange={ this.handleChange } value={ image.nsfw ? 1 : 0 } type='switch' name='nsfw' offLabel='SFW' onLabel='NSFW' />
+          </Row>
+        </div>
+
+        <div className='right-align'>
+          <Button type='submit'>Upload</Button>
+        </div>
+      </div>
+    </div>
+  }
+}
+
+UploadForm.propTypes = {
+  image: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string,
+    nsfw: PropTypes.boolean,
+    folder: PropTypes.string
+  }),
+  onChange: PropTypes.func
+}
+
+export default UploadForm
