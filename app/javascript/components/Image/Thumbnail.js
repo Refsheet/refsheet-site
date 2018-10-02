4db33fdbceb4dc05bc9451a58cef6c984c092f68
William Eisert <weisert@eisertdev.com>
2018-10-02 00:23:01 -0500

    Gallery moved to smarter component.

---
+++ app/javascript/components/Image/Thumbnail.js
@@ -0,0 +1,31 @@
+import react from 'react'
+import PropTypes from 'prop-types'
+
+const Thumbnail = ({image, style}) => {
+  console.log(image)
+
+  const { url, title, aspect_ratio } = image
+  const { small: src } = url
+
+  return (<div style={style} className='image-thumbnail black z-depth-1'>
+    { aspect_ratio
+      ? <img src={src} className='responsive-img block' alt={title} />
+      : <div className='message center-align caption margin-top--large'>Image processing...</div> }
+  </div>)
+}
+
+Thumbnail.propTypes = {
+  image: PropTypes.shape({
+    url: PropTypes.shape({
+      small: PropTypes.string.isRequired
+    }).isRequired,
+    size: PropTypes.shape({
+      small: PropTypes.shape({
+        width: PropTypes.number.isRequired,
+        height: PropTypes.number.isRequired
+      }).isRequired
+    }).isRequired
+  })
+}
+
+export default Thumbnail
