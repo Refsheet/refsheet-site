b6baad113a93cd005dc5a77626548adeac343362
Mau Abata <mauabata@gmail.com>
2018-10-04 21:24:56 -0500

    Return null also thumbnail stuff.

--- app/javascript/components/Image/Thumbnail.js
+++ app/javascript/components/Image/Thumbnail.js
@@ -1,14 +1,35 @@
-import react from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 const Thumbnail = ({image, style}) => {
-  const { url, title, aspect_ratio } = image
+  const { url, title, aspect_ratio, image_processing } = image
   const { small: src } = url
 
+  const renderImage = () => {
+    if (image_processing) {
+      return (<div className='message center-align'>
+        <div className='caption margin-top--large'>Image processing...</div>
+        <div className='muted margin-top--medium'>
+          Thumbnails are being generated for this image, and should be available soon.
+          You might need to reload this page.
+        </div>
+      </div>)
+
+    } else if (!aspect_ratio) {
+      return (<div className='message center-align'>
+        <div className='caption margin-top--large red-text'>Invalid image :(</div>
+        <div className='muted margin-top--medium'>
+          Something's not quite right. This might be a bug. Report Image #{image.id}.
+        </div>
+      </div>)
+
+    } else {
+      return (<img src={ src } className='responsive-img block' alt={ title }/>)
+    }
+  }
+
   return (<div style={style} className='image-thumbnail black z-depth-1'>
-    { aspect_ratio
-      ? <img src={src} className='responsive-img block' alt={title} />
-      : <div className='message center-align caption margin-top--large'>Image processing...</div> }
+    { renderImage() }
   </div>)
 }
 
@@ -22,7 +43,8 @@ Thumbnail.propTypes = {
         width: PropTypes.number.isRequired,
         height: PropTypes.number.isRequired
       }).isRequired
-    }).isRequired
+    }).isRequired,
+    image_processing: PropTypes.bool
   })
 }
 
