dff38d1c9fb1991a2374493e1a5f22ae9de27dcf
William Eisert <weisert@eisertdev.com>
2018-10-07 03:22:56 -0500

    Fake conversation list.

--- app/javascript/components/Chat/Conversations.js
+++ app/javascript/components/Chat/Conversations.js
@@ -2,15 +2,20 @@ import React from 'react'
 
 const Conversations = ({handleFormSubmit, messages}) => (
     <div className='chat-body conversations'>
-      <div className='card-content'>
-        Messages: {JSON.stringify(messages)}
-      </div>
-      <form onSubmit={handleFormSubmit} autoComplete="off" className='reply-box'>
-        <textarea name='message' className='browser-default margin--none min-height overline block' placeholder='Send a message...' />
-        <button type='submit' value='Send' className='btn btn-square'>
-          <Icon>send</Icon>
-        </button>
-      </form>
+      <ul className='chat-list'>
+        <li className='chat-conversation'>
+          <img src={'https://cloud.refsheet.net/images/images/000/065/797/medium/winterblack_alice_1.png?1528681529'} alt={'MauAbata'} />
+          <div className='time'>4:30pm</div>
+          <div className='title'>Mau Abata</div>
+          <div className='last-message'>So I was wondering, if you could maybe...</div>
+        </li>
+      </ul>
+      {/*<form onSubmit={handleFormSubmit} autoComplete="off" className='reply-box'>*/}
+        {/*<textarea name='message' className='browser-default margin--none min-height overline block' placeholder='Send a message...' />*/}
+        {/*<button type='submit' value='Send' className='btn btn-square'>*/}
+          {/*<Icon>send</Icon>*/}
+        {/*</button>*/}
+      {/*</form>*/}
     </div>
 )
 
