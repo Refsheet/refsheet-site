80e97818ecc48a62825e3338653cb58ba3bf99e3
Mau Abata <mauabata@gmail.com>
2018-10-11 05:16:42 -0500

    [f] We're SppOOoOOoooooOoky! I've also added some metadata to our emails, so Gmail can render fancy buttons.

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -10,27 +10,10 @@ module MailerHelper
     button_td_classes = []
     button_td_classes.push 'last' if last
 
-    capture do
-      concat content_tag(:table, border: 0, cellpadding: 0, cellspacing: 0, class: button_classes.join(' ')) {
-        content_tag :tbody do
-          content_tag :tr do
-            content_tag :td, align: 'left', class: button_td_classes.join(' ') do
-              content_tag :table, border: 0, cellpadding: 0, cellspacing: 0 do
-                content_tag :tbody do
-                  content_tag :tr do
-                    content_tag :td, link_to(text, path, { target: '_blank' }.merge(options))
-                  end
-                end
-              end
-            end
-          end
-        end
-      }
-
-      if primary
-        potential_action = case type.to_s
-          when "confirm"
-            <<-JSON
+    if primary
+      potential_action = case type.to_s
+        when "confirm"
+          <<-JSON
               {
                 "@type": "ConfirmAction",
                 "name": "#{escape_javascript text}",
@@ -39,9 +22,9 @@ module MailerHelper
                   "url": "#{path}"
                 }
               }
-            JSON
-          else
-            <<-JSON
+          JSON
+        else
+          <<-JSON
               {
                 "@type": "ViewAction",
                 "target": {
@@ -50,22 +33,39 @@ module MailerHelper
                 "url": "#{path}",
                 "name": "#{escape_javascript text}"
               }
-            JSON
-        end
+          JSON
+      end
 
-        concat content_tag :script, <<-JSON.squish, type: 'application/ld+json'
-          {
-            "@context": "http://schema.org",
-            "@type": "EmailMessage",
-            "description": "#{escape_javascript(options[:title] || text)}",
-            "potentialAction": #{potential_action},
-            "publisher": {
-              "@type": "Organization",
-              "name": "Refsheet.net",
-              "url": "https://refsheet.net"            
+      content_for(:head_script) do
+        content_tag :script, <<-JSON.squish, type: 'application/ld+json'
+            {
+              "@context": "http://schema.org",
+              "@type": "EmailMessage",
+              "description": "#{escape_javascript(options[:title] || text)}",
+              "potentialAction": #{potential_action},
+              "publisher": {
+                "@type": "Organization",
+                "name": "Refsheet.net",
+                "url": "https://refsheet.net"            
+              }
             }
-          }
         JSON
+      end
+    end
+
+    content_tag :table, border: 0, cellpadding: 0, cellspacing: 0, class: button_classes.join(' ') do
+      content_tag :tbody do
+        content_tag :tr do
+          content_tag :td, align: 'left', class: button_td_classes.join(' ') do
+            content_tag :table, border: 0, cellpadding: 0, cellspacing: 0 do
+              content_tag :tbody do
+                content_tag :tr do
+                  content_tag :td, link_to(text, path, { target: '_blank' }.merge(options))
+                end
+              end
+            end
+          end
+        end
       end
     end
   end
