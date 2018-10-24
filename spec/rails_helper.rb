93b3827804c703d7bf1c84d4f86829c05d00b205
William Eisert <weisert@eisertdev.com>
2018-10-23 19:58:49 -0500

    fix chrome spec?

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,7 +55,7 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-      chromeOptions: { args: %w(headless disable-gpu) }
+      chromeOptions: { args: %w(headless disable-gpu no-sandbox) }
   )
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
