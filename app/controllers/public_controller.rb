31c2538d2d424bc728044cd1da3194e126bb48fe
William Eisert <weisert@eisertdev.com>
2018-10-27 21:40:10 -0500

    Fixed Ahoy making a new visit on health checks omg that was painful.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,13 @@
 class PublicController < ApplicationController
+  skip_before_action :track_ahoy_visit, only: [:health]
+
   def health
+    # Ensure we have a connection to our database and that everything
+    # is functioning correctly:
+    Role.count >= 0
     head :ok
+  rescue => _e
+    head :internal_server_error
   end
 
   protected
