7ed4f831f4de128422efff10528f7d3b284151fa
William Eisert <weisert@eisertdev.com>
2018-10-28 10:23:06 -0500

    I shouldn't exclude ALL the ahoy.

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -4,7 +4,3 @@ end
 Ahoy.api = true
 Ahoy.user_agent_parser = :device_detector
 Ahoy.job_queue = :refsheet_application_queue
-
-Ahoy.exclude_method = lambda do |controller, request|
-  Rails.logger.debug({controller: controller, request: request}.inspect)
-end
