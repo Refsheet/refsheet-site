f03ed684c0cc41939c4c7c329e8f2a3220da12d6
Mau Abata <mauabata@gmail.com>
2018-10-28 03:16:43 -0500

    Improved /health endpoint. Again.

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,11 +1,15 @@
 class PublicController < ApplicationController
-  skip_before_action :track_ahoy_visit, only: [:health]
+  skip_before_action :track_ahoy_visit
+  skip_before_action :set_user_locale
+  skip_before_action :set_default_meta
+  skip_before_action :eager_load_session
+  skip_before_action :set_raven_context
 
   def health
     # Ensure we have a connection to our database and that everything
     # is functioning correctly:
-    Role.count >= 0
-    head :ok
+    counts = [User.count, Character.count, Image.count]
+    render json: { status: "OK", counts: counts, version: Refsheet::VERSION }, status: 200
   rescue => _e
     head :internal_server_error
   end
