e842a4e76cd4099fc33f1164d3a8a7bd142826e3
Mau Abata <mauabata@gmail.com>
2018-10-27 21:40:10 -0500

    Fixed Ahoy making a new visit on health checks omg that was painful.

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,10 +1,10 @@
 class Ahoy::Store < Ahoy::DatabaseStore
 end
 
-# set to true for JavaScript tracking
 Ahoy.api = true
-
-# better user agent parsing
 Ahoy.user_agent_parser = :device_detector
-
 Ahoy.job_queue = :refsheet_application_queue
+
+Ahoy.exclude_method = lambda do |controller, request|
+  Rails.logger.debug({controller: controller, request: request}.inspect)
+end
