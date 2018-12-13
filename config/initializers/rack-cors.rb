7b289a05191df4a21192c1fc401157fbf593b2e9
William Eisert <weisert@eisertdev.com>
2018-12-13 17:53:11 -0600

    Add extension to CORS.

--- config/initializers/rack-cors.rb
+++ config/initializers/rack-cors.rb
@@ -1,27 +1,38 @@
 ## Configure Rack CORS Middleware, so that CloudFront can serve our assets.
 ## See https://github.com/cyu/rack-cors
 
+def _cors(*urls)
+  urls.collect { |url| [
+      'https://' + url,
+      'http://' + url
+  ] }.flatten
+end
+
+CORS_PROD = _cors(
+    'refsheet.net',
+    'www.refsheet.net',
+    'ref.st'
+)
+
+CORS_EXT = _cors(
+    'extension.refsheet.net'
+)
+
+CORS_DEV = _cors(
+    'dev.refsheet.net',
+    '127.0.0.1',
+    'localhost'
+)
+
 if defined? Rack::Cors
   Rails.configuration.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins %w[
-          https://refsheet.net
-          http://refsheet.net
-          https://www.refsheet.net
-          http://www.refsheet.net
-          https://ref.st
-          http://ref.st
-          https://extension.refsheet.net
-          http://extension.refsheet.net
-      ]
+      origins CORS_PROD | CORS_DEV
       resource '/assets/*'
     end
 
     allow do
-      origins %w[
-          https://extension.refsheet.net
-          http://extension.refsheet.net
-      ]
+      origins CORS_PROD | CORS_EXT | CORS_DEV
       resource '/graphql', headers: :any, methods: [:get, :post, :options]
     end
   end
