27baa42b6f6528b062a1b8e2b90b0bfcb3df573f
William Eisert <weisert@eisertdev.com>
2019-03-02 13:14:19 -0800

    Fixed subscriptions!!!

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -9,31 +9,52 @@ module ApplicationCable
     protected
 
     def find_verified_user
-      if defined? cookies and cookies[UserSession::COOKIE_SESSION_TOKEN_NAME]
-        Rails.logger.info("Looking up user by #{cookies.signed[UserSession::COOKIE_SESSION_TOKEN_NAME].inspect}")
-        get_remembered_user
+      user = nil
+
+      if defined?(cookies)
+        Rails.logger.info("Cookies: #{cookies.to_h.inspect}")
       end
 
-      if defined? cookies and (user_id = cookies.signed[UserSession::COOKIE_USER_ID_NAME])
-        Rails.logger.info("Looking up user by user_id cookie: #{user_id}")
-        @current_user ||= User.find_by id: user_id
+      session_token = defined?(cookies) && cookies.signed[UserSession::COOKIE_SESSION_TOKEN_NAME]
+      insecure_user_id = defined?(cookies) && cookies.signed[UserSession::COOKIE_USER_ID_NAME]
+
+      if session_token
+        Rails.logger.info("Looking up user by session_token: #{session_token.inspect}")
+        user = get_remembered_user(session_token, insecure_user_id)
+      elsif insecure_user_id
+        Rails.logger.info("Looking up user by insecure_user_id: #{insecure_user_id.inspect}")
+        user = User.find_by id: user_id
+      else
+        Rails.logger.warn("Session Token and IUID is nil!")
+      end
+
+      Rails.logger.info("possible user: #{user}")
+
+      if user
+        user
       else
         reject_unauthorized_connection
       end
     end
 
-    def get_remembered_user
-      if cookies[UserSession::COOKIE_SESSION_ID_NAME] && cookies[UserSession::COOKIE_USER_ID_NAME]
-        session = UserSession.find_by(session_guid: cookies[UserSession::COOKIE_SESSION_ID_NAME])
+    def get_remembered_user(session_token, user_id)
+      session_id = cookies.signed[UserSession::COOKIE_SESSION_ID_NAME]
 
-        if session &&
-            session.user_id == cookies[UserSession::COOKIE_USER_ID_NAME] &&
-            session.authenticate(cookies[UserSession::COOKIE_SESSION_TOKEN_NAME])
-          Rails.logger.info("Authenticating user #{session.user_id} from stored session cookie.")
-          sign_in(session.user, remember: false)
-          return session.user
+      if session_id && user_id
+        session = UserSession.find_by(session_guid: session_id)
+
+        if session
+          Rails.logger.info("Session # #{session.id} found!")
+
+          if session.user_id == user_id && session.authenticate(session_token)
+            return session.user
+          end
+        else
+          Rails.logger.warn("No matching session found.")
         end
       end
+
+      false
     end
   end
 end
