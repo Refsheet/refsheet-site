0de0d7b642157a7d23377e252978745a02e680f6
William Eisert <weisert@eisertdev.com>
2019-04-19 17:06:10 -0700

    Rich text widgets can save now.

---
+++ app/javascript/components/Character/ProfileWidgetHeader.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class ProfileWidgetHeader extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  handleEditClick(e) {
+    e.preventDefault()
+    this.props.onEditStart()
+  }
+
+  handleCancelClick(e) {
+    e.preventDefault()
+    this.props.onEditStop()
+  }
+
+  handleSaveClick(e) {
+    e.preventDefault()
+    this.props.onSave()
+    // this.props.onEditStop()
+  }
+
+  renderLocked() {
+    const {widgetType, title, editable} = this.props
+
+    if (editable) {
+      return (
+        <div className='muted card-header'>
+          <div className='right' style={{opacity: 0.3}}>
+            <a href='#' className='margin-right--medium'><Icon className='muted' style={{fontSize: '1rem', color: 'rgba(255,255,255,0.1) !important'}}>delete</Icon></a>
+            <a href='#' onClick={this.handleEditClick.bind(this)}><Icon className='muted' style={{fontSize: '1rem', color: 'rgba(255,255,255,0.1) !important'}}>edit</Icon></a>
+          </div>
+          <div className='left' style={{opacity: 0.3}}>
+            <a href='#' className='margin-right--medium'><Icon className='muted' style={{fontSize: '1rem', color: 'rgba(255,255,255,0.1) !important'}}>reorder</Icon></a>
+            <a href='#'><Icon className='muted' style={{fontSize: '1rem', color: 'rgba(255,255,255,0.1) !important'}}>content_copy</Icon></a>
+          </div>
+
+          <div className='center'>
+            { title || widgetType }
+          </div>
+        </div>
+      );
+
+    } else if (title) {
+      return (
+        <div className='card-header'>
+          { title }
+        </div>
+      );
+
+    } else {
+      return null;
+    }
+  }
+
+  renderUnlocked() {
+    const {widgetType, title} = this.props
+
+    return (
+      <div className={'muted card-header'}>
+        <div className='right btn-group'>
+          <a href='#' className={'btn'} onClick={this.handleSaveClick.bind(this)}>
+            <Icon className='muted left' style={{fontSize: '1rem', color: 'rgba(255,255,255,0.1) !important'}}>save</Icon> Save
+          </a>
+        </div>
+
+        <div className={'center'}>
+          { title || widgetType }
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    if (this.props.editing) return this.renderUnlocked()
+    else return this.renderLocked()
+  }
+}
+
+ProfileWidgetHeader.propTypes = {
+  editable: PropTypes.bool,
+  editing: PropTypes.bool,
+  onEditStart: PropTypes.func.isRequired,
+  onSave: PropTypes.func.isRequired,
+  onEditStop: PropTypes.func.isRequired
+}
+
+export default ProfileWidgetHeader
--- app/javascript/components/Character/Widgets/RichTextWidget.js
+++ app/javascript/components/Character/Widgets/RichTextWidget.js
@@ -1,17 +1,96 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import MarkdownInput from '@opuscapita/react-markdown'
+import c from 'classnames'
 
 class RichTextWidget extends Component {
   constructor(props) {
     super(props)
+  }
 
-    this.state = {
-      editable: false,
-    }
+  handleMarkdownChange(content) {
+    this.props.onChange({content})
   }
 
   render() {
-    const {content, contentHtml, title} = this.props
+    const {contentHtml} = this.props
+
+    if (this.props.editing) {
+      const usernameAutoComplete = {
+        specialCharacter: '@',
+        termRegex: /^@(\w*)$/,
+        searchItems(term) {
+          const items = [
+            {username: 'mauabata', displayName: 'Mau Abata'},
+            {username: 'inkmaven', displayName: 'Inkmaven'}
+          ];
+
+          return new Promise(resolve => {
+            const filtered = items.filter(({ username }) =>
+              username.indexOf(term.substring(1)) === 0
+            );
+
+            setTimeout(_ => resolve(filtered), 300);
+          })
+        },
+        markdownText(item) {
+          return '@' + item.username;
+        },
+        renderItem: ({ item, isSelected }) => (
+          <div className={c('react-markdown--autocomplete-widget__item user-token', {selected: isSelected})}>
+            <span className={'name'}>{ item.displayName }</span>
+            <span className={'username'}>@{item.username}</span>
+          </div>
+        )
+      }
+
+      const characterAutoComplete = {
+        specialCharacter: '@',
+        termRegex: /^@(\w*)\/(\w*)$/,
+        searchItems(term) {
+          const items = [
+            {username: 'mauabata', displayName: 'Alice Nikova', slug: 'alice'},
+            {username: 'inkmaven', displayName: 'Ink', slug: 'ink'}
+          ];
+
+          const search = term.split('/')[1]
+          console.log({search})
+
+          return new Promise(resolve => {
+            const filtered = items.filter(({ slug }) =>
+              slug.indexOf(search) === 0
+            );
+
+            setTimeout(_ => resolve(filtered), 300);
+          })
+        },
+        markdownText(item) {
+          return '@' + item.username + '/' + item.slug;
+        },
+        renderItem: ({ item, isSelected }) => (
+          <div className={c('react-markdown--autocomplete-widget__item user-token character', {selected: isSelected})}>
+            <span className={'name'}>{ item.displayName }</span>
+            <span className={'username'}>@{item.username}/{item.slug}</span>
+          </div>
+        )
+      }
+
+      return (
+        <div className={'rich-text-widget editing'}>
+          <MarkdownInput
+            value={this.props.content || 'Enter text here...'}
+            showFullScreenButton={false}
+            onChange={this.handleMarkdownChange.bind(this)}
+            onBlur={() => console.log("md blur")}
+            autoFocus={true}
+            extensions={[
+              usernameAutoComplete,
+              characterAutoComplete
+            ]}
+          />
+        </div>
+      )
+    }
 
     return (
       <div className='rich-text-widget'>
@@ -27,7 +106,9 @@ class RichTextWidget extends Component {
 
 RichTextWidget.propTypes = {
   content: PropTypes.string,
-  contentHtml: PropTypes.string
+  contentHtml: PropTypes.string,
+  editing: PropTypes.bool,
+  onChange: PropTypes.func.isRequired
 }
 
 export default RichTextWidget
