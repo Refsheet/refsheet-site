4f0734cb3500ce51bc8adc636a0d30a0c16e6d15
Mau Abata <mauabata@gmail.com>
2019-04-22 10:57:45 -0700

    Create profile sections and render in the proper order.

--- app/graphql/types/profile_section_type.rb
+++ app/graphql/types/profile_section_type.rb
@@ -5,4 +5,5 @@ Types::ProfileSectionType = GraphQL::ObjectType.define do
   field :columns, !types[types.Int]
   field :title, types.String
   field :widgets, types[Types::WidgetType]
+  field :row_order, types.Int
 end
