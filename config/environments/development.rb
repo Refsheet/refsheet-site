6fbabf85c951743adce13cb4434285bc44a40698
William Eisert <weisert@eisertdev.com>
2019-05-02 16:10:15 -0700

    Log to Google Cloud

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,8 @@ Rails.application.configure do
   config.webpacker.check_yarn_integrity = false
 
   # Settings specified here will take precedence over those in config/application.rb.
-  config.logger = ActiveSupport::Logger.new(STDOUT)
+  config.logger = Refsheet::Logger.new(STDOUT)
+  config.log_tags = [:request_id]
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
