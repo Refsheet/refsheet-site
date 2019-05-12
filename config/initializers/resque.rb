5085d78b5bb78dc7a5d5ef0bef37873bd2498e22
William Eisert <weisert@eisertdev.com>
2019-05-11 18:48:03 -0700

    Development docker container runs!

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,8 +1,22 @@
-if Rails.env.development?
-  Resque.logger = ActiveSupport::Logger.new(STDOUT)
-  Resque.logger.formatter = Resque::VerboseFormatter.new
-  Resque.logger.level = Logger::DEBUG
-  puts Resque.logger.inspect
-  puts Rails.logger.inspect
-  Rails.logger.info("Resque initialized.")
+Rails.application.configure do
+  if Rails.env.development?
+    Resque.logger = ActiveSupport::Logger.new(STDOUT)
+    Resque.logger.formatter = Resque::VerboseFormatter.new
+    Resque.logger.level = Logger::DEBUG
+  end
+
+  resque_config = config_for(:resque)
+  password = ""
+  channel = ""
+
+  if resque_config['password'].present?
+    password = ":%s@" % resque_config['password']
+  end
+
+  Resque.redis = "redis://%s%s:%s%s" % [
+      password,
+      resque_config['host'],
+      resque_config['port'],
+      channel
+  ]
 end
