ee09a8c2c9ff4699070fce3388b0e7fee7c8c4e0
William Eisert <weisert@eisertdev.com>
2019-07-02 09:05:46 -0700

    Forum improvements and translations.

--- app/javascript/components/App/Router.js
+++ app/javascript/components/App/Router.js
@@ -2,14 +2,28 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Character from 'Character'
 import Moderate from 'Moderate'
+import Forums from 'Forums'
+import Forum from "Forums/Forum";
+
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/v2/:username/:slug" component={ Character } />
-      <Route path={"/:username/:slug"} component={ Character } />
+      {/** Forums **/}
+      <Route path={"/v2/forums/:id"} component={ Forum } />
+      <Route path={"/v2/forums"} component={ Forums } />
+      {/*<Route path={"/forums/new"} component={ NewForum } />*/}
+      {/*<Route path={"/forums/:id/settings"} component={ EditForum } />*/}
+      {/*<Route path={"/forums/:forumId/:id"} component={ Discussion } />*/}
+
+      {/** Moderation **/}
       <Route path="/moderate" component={ Moderate } />
 
+      {/** Character Profiles **/}
+      <Route path="/v2/:username/:slug" component={ Character } />
+      <Route path={"/:username/:slug"} component={ Character } />
+
+      {/** Misc **/}
       <Route path="*" component={ NotFound } />
     </Switch>
   </BrowserRouter>
