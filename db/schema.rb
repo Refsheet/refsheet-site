c83415b0cda5f58a5aa22e463a37e094ef204c1c
William Eisert <weisert@eisertdev.com>
2019-08-07 22:19:52 -0700

    Wrap up spporter changes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190805221708) do
+ActiveRecord::Schema.define(version: 20190808050615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -851,8 +851,8 @@ ActiveRecord::Schema.define(version: 20190805221708) do
     t.string   "email"
     t.string   "password_digest"
     t.text     "profile"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
@@ -865,6 +865,7 @@ ActiveRecord::Schema.define(version: 20190805221708) do
     t.datetime "email_confirmed_at"
     t.datetime "deleted_at"
     t.boolean  "avatar_processing"
+    t.integer  "support_pledge_amount", default: 0
     t.index ["deleted_at"], name: "index_users_on_deleted_at", using: :btree
     t.index ["parent_user_id"], name: "index_users_on_parent_user_id", using: :btree
     t.index ["type"], name: "index_users_on_type", using: :btree
