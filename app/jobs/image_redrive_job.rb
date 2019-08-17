38bf70aa3bbc0d8c6f742c55a713759d0d2ef073
William Eisert <weisert@eisertdev.com>
2019-08-17 16:14:47 -0700

    Image redrive job updates.

--- app/jobs/image_redrive_job.rb
+++ app/jobs/image_redrive_job.rb
@@ -1,7 +1,7 @@
 # Reprocesses images that have not been processed within the last hour:
 #
 class ImageRedriveJob < ApplicationJob
-  def perform(deep: false, skip_pending: false)
+  def perform(deep: false, skip_pending: false, deep_limit: nil, deep_start: nil)
     unless skip_pending
       Image.processing.where('images.image_updated_at < ?', 1.hour.ago).limit(1000).each do |img|
         Rails.logger.info("Reprocessing image #{img.guid} (#{img.id})", image_updated_at: img.image_updated_at)
@@ -14,11 +14,21 @@ class ImageRedriveJob < ApplicationJob
       cloud = Net::HTTP.new('cloud.refsheet.net', 443)
       cloud.use_ssl = true
 
-      Image.processed.order(created_at: :desc).find_each do |img|
+      scope = Image.processed.order(created_at: :desc)
+
+      if deep_limit
+        scope = scope.limit(deep_limit)
+      end
+
+      if deep_start
+        scope = scope.where('images.id <= ?', deep_start)
+      end
+
+      scope.find_each do |img|
         res = cloud.request_head(img.image.path)
-        if res.code != 200
+        if res.code != "200"
           Rails.logger.info("Image #{img.guid} (#{img.id}) corrupt, reprocessing...")
-          img.image.reprocess!
+          img.image.update_columns(image_processing: true)
         else
           Rails.logger.info("Image #{img.guid} (#{img.id}) OK")
         end
