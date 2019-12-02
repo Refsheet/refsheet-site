d3c327d8a3effc0bf8de53a440173f50e2495040
William Eisert <weisert@eisertdev.com>
2019-12-01 17:39:00 -0800

    Add media comment mutations.

---
+++ app/graphql/mutations/media_comment_mutations.rb
@@ -0,0 +1,34 @@
+class Mutations::MediaFavoriteMutations < Mutations::ApplicationMutation
+  before_action :get_media
+
+  action :create do
+    type Types::MediaCommentType
+    argument :mediaId, !types.ID
+    argument :comment, !types.String
+  end
+
+  def create
+    @comment = Media::Comment.create media: @media,
+                                     user: context.current_user.call,
+                                     comment: params[:comment]
+  end
+
+  action :destroy do
+    type Types::MediaCommentType
+    argument :mediaId, !types.ID
+    argument :id, !types.ID
+  end
+
+  def destroy
+    @comment = @media.comments.find_by! guid: params[:id]
+    authorize! @media.managed_by?(current_user) || @comment.user == current_user
+    @comment.destroy
+    @comment
+  end
+
+  private
+
+  def get_media
+    @media = Image.find_by! guid: params[:mediaId]
+  end
+end
