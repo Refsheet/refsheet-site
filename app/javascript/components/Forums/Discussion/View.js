e7f53b911b2e169a2ccd3f361e21468c4e817618
Mau Abata <mauabata@gmail.com>
2019-12-29 11:34:50 -0800

    Discussion view views, but I really need some design inspiration.

--- app/javascript/components/Forums/Discussion/View.js
+++ app/javascript/components/Forums/Discussion/View.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
 import compose from '../../../utils/compose'
-import Jumbotron from '../../Shared/Jumbotron'
-import Main from '../../Shared/Main'
-import { Link } from 'react-router-dom'
 
 class View extends Component {
   render() {
@@ -11,27 +8,16 @@ class View extends Component {
     console.log({ discussion })
 
     return (
-      <Main title={'Forums'} className={'main-flex split-bg-right'}>
-        <Jumbotron short>
+      <div className={'container container-flex'}>
+        <main className={'content-left'}>
           <h1>{discussion.topic}</h1>
-          <p>
-            Posted in:{' '}
-            <Link to={`/v2/forums/${discussion.forum.slug}`}>
-              {discussion.forum.name}
-            </Link>
-          </p>
-        </Jumbotron>
+          <div className={'forum-post--content'}>{discussion.content}</div>
+        </main>
 
-        <div className={'container container-flex'}>
-          <main className={'content-left'}>
-            <div className={'forum-post--content'}>{discussion.content}</div>
-          </main>
-
-          <aside className={'sidebar left-pad'}>
-            {typeof Advertisement != 'undefined' && <Advertisement />}
-          </aside>
-        </div>
-      </Main>
+        <aside className={'sidebar left-pad'}>
+          {typeof Advertisement != 'undefined' && <Advertisement />}
+        </aside>
+      </div>
     )
   }
 }
