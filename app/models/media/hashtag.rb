dbf3221d82a9bf14e8d6be3c16737d907f0f3b78
William Eisert <weisert@eisertdev.com>
2020-01-01 11:59:29 -0800

    Restore HTML in image lightbox.

--- app/models/media/hashtag.rb
+++ app/models/media/hashtag.rb
@@ -11,7 +11,13 @@
 class Media::Hashtag < ApplicationRecord
   include NamespacedModel
 
+  has_and_belongs_to_many :media, class_name: "Image", foreign_key: :media_hashtag_id
+
   def self.[](tag)
     find_or_initialize_by tag: tag
+  end
+
+  def count
+    media.count
   end
 end
