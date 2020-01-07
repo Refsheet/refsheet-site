1ed95385e005b79017c5ccfb54c2eaa6b2b0eee0
William Eisert <weisert@eisertdev.com>
2020-01-07 14:33:17 -0800

    Tabs and shelf.

---
+++ app/javascript/components/Character/Modals/ColorModal/AdvancedSchemeForm.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import compose from 'utils/compose'
+
+class AdvancedSchemeForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render() {
+    return <h1>AdvancedSchemeForm</h1>
+  }
+}
+
+AdvancedSchemeForm.propTypes = {}
+
+export default compose(
+  // TODO: Add HOC bindings here
+)(AdvancedSchemeForm)
