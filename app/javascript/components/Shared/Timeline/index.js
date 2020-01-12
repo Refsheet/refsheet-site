ac497c8a8c394d83989e92b6dd9a99c3ad059582
William Eisert <weisert@eisertdev.com>
2020-01-12 07:44:52 -0800

    Timeline uses collapsible module.

---
+++ app/javascript/components/Shared/Timeline/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Timeline = ({children}) => {
+  return (
+    <ul className={'timeline'}>
+      { children }
+    </ul>
+  )
+}
+
+Timeline.propTypes = {}
+
+export default Timeline
