401a4bcecf0c035f9e7814d26a006a907d8f376f
William Eisert <weisert@eisertdev.com>
2020-01-12 08:52:54 -0800

    Add versions to character schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_224208) do
+ActiveRecord::Schema.define(version: 2020_01_12_162747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -928,9 +928,9 @@ ActiveRecord::Schema.define(version: 2020_01_07_224208) do
     t.bigint "item_id", null: false
     t.string "event", null: false
     t.string "whodunnit"
-    t.text "object"
     t.datetime "created_at"
-    t.text "object_changes"
+    t.jsonb "object"
+    t.jsonb "object_changes"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
