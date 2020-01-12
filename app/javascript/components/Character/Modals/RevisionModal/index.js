dd930312fc1f649fd9949791a50b36c6189831fa
Mau Abata <mauabata@gmail.com>
2020-01-12 07:44:52 -0800

    Timeline uses collapsible module.

--- app/javascript/components/Character/Modals/RevisionModal/index.js
+++ app/javascript/components/Character/Modals/RevisionModal/index.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import compose from 'utils/compose'
 import { Icon } from 'react-materialize'
-import {withNamespaces} from "react-i18next";
+import {Trans, withNamespaces} from "react-i18next";
 import Moment from "react-moment";
 import c from 'classnames'
 import Diff from 'react-stylable-diff'
+import TimelineEntry from "../../../Shared/Timeline/TimelineEntry";
 
 class RevisionModal extends Component {
   constructor(props) {
@@ -14,6 +15,10 @@ class RevisionModal extends Component {
     this.state = {
       activeVersion: 0
     }
+  }
+
+  componentDidUpdate() {
+    M.Collapsible.init(this.collapsible)
   }
 
   handleVersionClick(version) {
@@ -40,22 +45,20 @@ class RevisionModal extends Component {
         title={t("labels.character_revisions", "Character Revisions")}
         onClose={this.props.onClose}
       >
-        <p>
+        <Trans i18nKey={'captions.character_revisions'} parent={'p'}>
           This is a list of recent changes to your character's information. Here you can view your character at
           previous points in time, and roll back to undo changes.
-        </p>
+        </Trans>
 
-        <ul className={'timeline'}>
+        <ul className={'timeline collapsible accordion'} ref={r => this.collapsible = r}>
           {[0,1,2,3,4,5,6].map((i) =>
-          <li key={i} className={c('timeline-entry', { current: this.state.activeVersion === i })} onClick={this.handleVersionClick(i).bind(this)}>
-            <Moment format={"L"} withTitle titleFormat={"L LT"} />
-            <div className={'title'}>
-              Update
-            </div>
-
-            You updated 9 fields
-
-            { this.state.activeVersion === i && <div className={'details'}>
+          <TimelineEntry key={i}
+                         current={ this.state.activeVersion === i }
+                         onClick={this.handleVersionClick(i).bind(this)}
+                         time={123456789123}
+                         title={"Update"}
+                         summary={"You updated 2 fields"}
+          >
               <div className={'change-table'}>
                 { Object.keys(changes).map(changeKey => <div className={'change'}>
                   <div className={'label'}>{ changeKey }</div>
@@ -64,8 +67,7 @@ class RevisionModal extends Component {
                   {/*<div className={'to'}>{ changes[changeKey][1] }</div>*/}
                 </div>)}
               </div>
-            </div>}
-          </li> )}
+          </TimelineEntry> )}
         </ul>
       </Modal>
     )
