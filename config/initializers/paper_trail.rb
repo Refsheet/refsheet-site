8cbc898c4da41992902a4385177b8c6907e9d2f8
Mau Abata <mauabata@gmail.com>
2020-01-13 18:51:45 -0800

    Try removing paper_trail require hack?

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
   # HACK: It seems like the model isn't being picked up in development mode unless this
   #       require statement is here. Otherwise, it defaults to the old class.
-  require_relative '../../app/models/paper_trail/version'
+  #require_relative '../../app/models/paper_trail/version'
 end
