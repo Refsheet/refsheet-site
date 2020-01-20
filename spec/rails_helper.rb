0b4a417080f0e48f6280e0656ab18eca61a14e43
Mau Abata <mauabata@gmail.com>
2020-01-20 14:40:54 -0800

    Retry in CI.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,7 +36,7 @@ RSpec.configure do |config|
   config.verbose_retry = true
   config.display_try_failure_messages = true
 
-  if ENV['CIRCLECI']
+  if ENV['CI']
     config.around :each, :js do |ex|
       ex.run_with_retry retry: 3
     end
