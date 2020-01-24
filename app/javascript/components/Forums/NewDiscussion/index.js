8fc0428f99f46309498271141d2ae135272d4619
Mau Abata <mauabata@gmail.com>
2020-01-23 23:09:16 -0800

    Other Things.

--- app/javascript/components/Forums/NewDiscussion/index.js
+++ app/javascript/components/Forums/NewDiscussion/index.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import compose from 'utils/compose'
+import { withCurrentUser } from '../../../utils/compose'
+import { H1 } from '../../Styled/Headings'
+import { withNamespaces } from 'react-i18next'
 
 class NewDiscussion extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { discussion: {} }
+    this.state = { post: {} }
   }
 
   render() {
-    return <h1>NewDiscussion</h1>
+    const { forum, t } = this.props
+
+    return (
+      <div className={'container container-flex'}>
+        <main className={'content-left padding-bottom--large'}>
+          <H1>New Discussion</H1>
+        </main>
+
+        <aside className={'sidebar left-pad'}>
+          {typeof Advertisement != 'undefined' && <Advertisement />}
+        </aside>
+      </div>
+    )
   }
 }
 
-NewDiscussion.propTypes = { forum: PropTypes.object.isRequired }
+NewDiscussion.propTypes = {
+  forum: PropTypes.object.isRequired,
+}
 
-export default compose()(NewDiscussion)
-// TODO: Add HOC bindings here
+export default compose(
+  withNamespaces('common'),
+  withCurrentUser()
+)(NewDiscussion)
