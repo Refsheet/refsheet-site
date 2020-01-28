5fe643d0b34540464f7ef8dd70d8a13ee4130458
William Eisert <weisert@eisertdev.com>
2020-01-28 03:00:31 -0800

    Attempt to upgrade to Rails 6?

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,12 +12,12 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w[
-  serviceworker.js
-  manifest.json
-  admin.css
-  admin.js
-]
+# Rails.application.config.assets.precompile += %w[
+#   serviceworker.js
+#   manifest.json
+#   admin.css
+#   admin.js
+# ]
 
 # Disable GZip because Google Cloud is going to do that for us?
 Rails.application.config.assets.gzip = false
