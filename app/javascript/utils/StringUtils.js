a8b95919b46e59444dd21164702ca1ac71a74aac
Mau Abata <mauabata@gmail.com>
2020-03-09 19:02:13 -0700

    Aha! It renders!

---
+++ app/javascript/utils/StringUtils.js
@@ -0,0 +1,84 @@
+/**
+ * Various string manipulation functions, mostly for handling camelization/unCamelization of object keys.
+ *
+ * @example
+ *    StringUtils.camelize("i_am_snek") // => "iAmSnek"
+ *    StringUtils.camelizeKeys({ foo_bar: "baz"}) // => { fooBar: "baz" }
+ */
+class StringUtils {
+  /**
+   * Camelize a string.
+   * @param string {string} - i_am_a_string
+   * @returns {string} - iAmAString
+   */
+  static camelize(string) {
+    return string
+      .toLowerCase()
+      .replace(/_(.)/g, $1 => $1.toUpperCase())
+      .replace(/_/g, '')
+  }
+
+  /**
+   * Camelize object keys. This is not recursive, only the top level will be camelized.
+   * @param object {object} - { i_am: "an object" }
+   * @returns {object} - { iAm: "an object" }
+   */
+  static camelizeKeys(object) {
+    const out = {}
+
+    for (let k in object) {
+      const v = object[k]
+      out[this.camelize(k)] = v
+    }
+
+    return out
+  }
+
+  /**
+   * Un-camelize a string.
+   * @param string {string} - iAmAString
+   * @returns {string} - i_am_a_string
+   */
+  static unCamelize(string) {
+    return string.replace(
+      /([a-z])([A-Z])/g,
+      (a, $0, $1) => $0 + '_' + $1.toLowerCase()
+    )
+  }
+
+  /**
+   * Un-camelize an object. This is also not recursive.
+   * @param object {object} - { iAm: "an object" }
+   * @returns {object} - { i_am: "an object" }
+   */
+  static unCamelizeKeys(object) {
+    const out = {}
+
+    for (let k in object) {
+      const v = object[k]
+      out[this.unCamelize(k)] = v
+    }
+
+    return out
+  }
+
+  /**
+   * Camelize an object, but leave the snake case strings as well. This is more of a lazy hack than a real solution,
+   * and should likely be avoided. Please. Thank you.
+   * @param object {object} - { i_am: "an object" }
+   * @returns {object} - { i_am: "an object", iAm: "an object" }
+   */
+  static indifferentKeys(object) {
+    const out = {}
+
+    for (let k in object) {
+      const v = object[k]
+      out[k] = v
+      out[this.camelize(k)] = v
+    }
+
+    return out
+  }
+}
+
+export default StringUtils
