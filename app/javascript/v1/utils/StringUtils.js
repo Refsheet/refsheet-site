f65631fce9cc86b3c5677485049ac4c16c81fe39
William Eisert <weisert@eisertdev.com>
2020-03-09 17:26:37 -0700

    We're up to the final render.

--- app/javascript/v1/utils/StringUtils.js
+++ app/javascript/v1/utils/StringUtils.js
@@ -5,60 +5,57 @@
  * DS206: Consider reworking classes to avoid initClass
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const Cls = (this.StringUtils = class StringUtils {
-  static initClass() {
-    this.camelize = string => {
-      //
-      // by http://stackoverflo.com/users/140811/scott
-      // at http://stackoverflow.com/a/2970588/6776673
+class StringUtils {
+  static camelize(string) {
+    //
+    // by http://stackoverflow.com/users/140811/scott
+    // at http://stackoverflow.com/a/2970588/6776673
 
-      return string
-        .toLowerCase()
-        .replace(/_(.)/g, $1 => $1.toUpperCase())
-        .replace(/_/g, '')
-    }
-
-    this.camelizeKeys = object => {
-      const out = {}
-
-      for (let k in object) {
-        const v = object[k]
-        out[this.camelize(k)] = v
-      }
-
-      return out
-    }
-
-    this.unCamelize = string => {
-      return string.replace(
-        /([a-z])([A-Z])/g,
-        (a, $0, $1) => $0 + '_' + $1.toLowerCase()
-      )
-    }
-
-    this.unCamelizeKeys = object => {
-      const out = {}
-
-      for (let k in object) {
-        const v = object[k]
-        out[this.unCamelize(k)] = v
-      }
-
-      return out
-    }
-
-    this.indifferentKeys = object => {
-      const out = {}
-
-      for (let k in object) {
-        const v = object[k]
-        out[k] = v
-        out[this.camelize(k)] = v
-      }
-
-      return out
-    }
+    return string
+      .toLowerCase()
+      .replace(/_(.)/g, $1 => $1.toUpperCase())
+      .replace(/_/g, '')
   }
-})
-Cls.initClass()
-export default this.StringUtils
+
+  static camelizeKeys(object) {
+    const out = {}
+
+    for (let k in object) {
+      const v = object[k]
+      out[this.camelize(k)] = v
+    }
+
+    return out
+  }
+
+  static unCamelize(string) {
+    return string.replace(
+      /([a-z])([A-Z])/g,
+      (a, $0, $1) => $0 + '_' + $1.toLowerCase()
+    )
+  }
+
+  static unCamelizeKeys(object) {
+    const out = {}
+
+    for (let k in object) {
+      const v = object[k]
+      out[this.unCamelize(k)] = v
+    }
+
+    return out
+  }
+
+  static indifferentKeys(object) {
+    const out = {}
+
+    for (let k in object) {
+      const v = object[k]
+      out[k] = v
+      out[this.camelize(k)] = v
+    }
+
+    return out
+  }
+}
+export default StringUtils
