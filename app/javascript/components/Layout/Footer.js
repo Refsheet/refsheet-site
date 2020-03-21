9a9d8ed3ca93926caf1bddf56753c1c40d1d24d6
William Eisert <weisert@eisertdev.com>
2020-03-21 05:53:06 -0500

    Update Footer.js

--- app/javascript/components/Layout/Footer.js
+++ app/javascript/components/Layout/Footer.js
@@ -101,6 +101,20 @@ class _Footer extends Component {
               </p>
             </Col>
           </Row>
+    
+          <div class="margin-top--large">&nbsp;</div>
+    
+          <!-- Footer Support Box -->
+          <ins class="adsbygoogle"
+               style="display:block"
+               data-ad-client="ca-pub-4929509110499022"
+               data-ad-slot="9500968119"
+               data-ad-format="auto"
+               data-full-width-responsive="true"></ins>
+    
+          <script>
+               (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>
 
           <hr />
 
