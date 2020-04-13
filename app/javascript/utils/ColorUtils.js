5799bbb36f29c37f02159185184690cec1479e1f
Mau Abata <mauabata@gmail.com>
2020-04-13 02:36:55 -0500

    Try converting V1 data to V2 on the fly.

--- app/javascript/utils/ColorUtils.js
+++ app/javascript/utils/ColorUtils.js
@@ -2,10 +2,17 @@ import Color from 'color'
 
 const ColorUtils = {
   convertV1(v1ColorData) {
-    console.log({ v1ColorData })
-    return {
-      ...v1ColorData,
-    }
+    let v2ColorData = { ...v1ColorData }
+
+    Object.keys(v1ColorData).map(k => {
+      if (/-/.test(k)) {
+        v2ColorData[
+          k.replace(/([a-z])-([a-z])/g, ($1, $2) => `${$1}${$2.toUpperCase()}`)
+        ] = v1ColorData[k]
+      }
+    })
+
+    return v2ColorData
   },
 }
 
