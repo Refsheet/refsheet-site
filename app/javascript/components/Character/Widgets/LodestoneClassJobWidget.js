3f48af117a0c197008c26a4cfe877806a031b881
William Eisert <weisert@eisertdev.com>
2020-05-01 05:10:32 -0500

    AHA! We have CLASSJOBS!
    Signed-off-by: William Eisert <weisert@eisertdev.com>

--- app/javascript/components/Character/Widgets/LodestoneClassJobWidget.js
+++ app/javascript/components/Character/Widgets/LodestoneClassJobWidget.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Attribute from '../../../v1/shared/attributes/attribute'
-import AttributeTable from 'v1/shared/attributes/attribute_table'
-import StringUtils from '../../../utils/StringUtils'
-import * as ObjectUtils from 'utils/ObjectUtils'
+import { Row, Col } from 'react-materialize'
+import Muted, { TextLight } from '../../Styled/Muted'
+import { Caption, MutedCaption } from '../../Styled/Caption'
+import c from 'classnames'
 
 class LodestoneClassJobWidget extends Component {
   render() {
@@ -11,17 +11,42 @@ class LodestoneClassJobWidget extends Component {
       character: { lodestone_character },
     } = this.props
 
-    const flat = ObjectUtils.flatten(
-      ObjectUtils.deepRemoveKeys(lodestone_character, '__typename')
-    )
+    if (!lodestone_character) {
+      return (
+        <Caption className={'center card-content'}>No Lodestone Link!</Caption>
+      )
+    }
+
+    let class_jobs = [...lodestone_character.class_jobs]
+    class_jobs.sort((a, b) => b.level - a.level)
 
     return (
-      <div className={'card-content'}>
-        <AttributeTable>
-          {Object.keys(flat).map(k => (
-            <Attribute key={k} name={StringUtils.humanize(k)} value={flat[k]} />
+      <div className={'card-content padding-bottom--none'}>
+        <Row>
+          {class_jobs.map(cj => (
+            <Col key={cj.id} s={3} m={2}>
+              <div className={c('center-align', { faded: cj.level === 0 })}>
+                <img
+                  width={50}
+                  style={{ margin: '0 auto' }}
+                  className={'block responsive-img'}
+                  src={cj.class_icon_url}
+                  alt={cj.name}
+                />
+                {cj.level > 0 ? (
+                  <Caption className={'larger'}>{cj.level}</Caption>
+                ) : (
+                  <MutedCaption className={'larger'}>{cj.level}</MutedCaption>
+                )}
+                <TextLight>
+                  <abbr title={cj.job_active ? cj.job_name : cj.class_name}>
+                    {cj.job_active ? cj.job_abbr : cj.class_abbr}
+                  </abbr>
+                </TextLight>
+              </div>
+            </Col>
           ))}
-        </AttributeTable>
+        </Row>
       </div>
     )
   }
