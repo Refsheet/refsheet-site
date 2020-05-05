aed3ecdf624f11b4dab9fb0f25e78abdc50172e5
William Eisert <weisert@eisertdev.com>
2020-05-05 17:50:07 -0500

    Add fields to graphql guestbook type.
    Signed-off-by: William Eisert <weisert@eisertdev.com>

--- app/graphql/types/guestbook_entry_type.rb
+++ app/graphql/types/guestbook_entry_type.rb
@@ -3,6 +3,9 @@ module Types
     name 'GuestbookEntry'
     interfaces [Interfaces::ApplicationRecordInterface]
 
-
+    field :character, Types::CharacterType
+    field :author, Types::UserType
+    field :author_character, Types::CharacterType
+    field :message, types.String
   end
 end
