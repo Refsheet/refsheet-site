4afe3b15dc08825ef2d96f99254d81fdd65ff01c
Mau Abata <mauabata@gmail.com>
2021-01-31 13:57:11 -0600

    Try reporting source maps with Sentry deploys.

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,8 +1,20 @@
 const environment = require('./environment')
+const SentryWebpackPlugin = require('@sentry/webpack-plugin')
 
 environment.config.merge({
   devtool: 'cheap-module-source-map'
 })
+
+environment.plugins.append('sentry-webpack', new SentryWebpackPlugin({
+  // sentry-cli configuration
+  authToken: process.env.SENTRY_API_TOKEN,
+  org: "refsheetnet",
+  project: "refex",
+
+  // webpack specific configuration
+  include: ".",
+  ignore: ["node_modules", "webpack.config.js"],
+}))
 
 // webpack-5
 environment.plugins.delete('OptimizeCSSAssets')
