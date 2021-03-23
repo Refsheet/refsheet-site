0b5558c3a3ed14045438893ceecfadda32ef63eb
Mau Abata <mauabata@gmail.com>
2021-03-23 13:33:30 -0500

    Re-enable HTML presets.

--- app/javascript/utils/sanitize.js
+++ app/javascript/utils/sanitize.js
@@ -3,6 +3,11 @@ import DOMPurify from 'dompurify'
 export function sanitize(html) {
   return DOMPurify.sanitize(html, {
     ALLOWED_ATTR: ['style', 'class', 'type', 'href'],
+    USE_PROFILES: {
+      html: true,
+      mathml: true,
+      svg: true,
+    },
     ALLOWED_TAGS: [
       'figure',
       'table',
