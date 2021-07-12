af3d0f82e152aa7045cd0725fa648aa2675adfd4
Mau Abata <69772612+MauAbata@users.noreply.github.com>
2021-07-12 14:09:31 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
             },
             title: @user.name,
             description: @user.profile.presence || 'This user is a mystery!',
-            image_src: @user.avatar_url(:medium)
+            image_src: @user.avatar_url(:medium),
+            robots: 'noindex',
         )
 
         eager_load user: UserSerializer.new(@user, scope: view_context).as_json
